f383ca448dd7a00bed9853a144a6867f70bcaac0
Wdev <wrlandev0@gmail.com>
2025-01-18 00:22:25 -0300

    feat:add mongodb and rabbitmq configurations

---
+++ src/main/java/tech/wdev/orderms/config/RabiitMqConfig.java
@@ -0,0 +1,9 @@
+package tech.wdev.orderms.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabiitMqConfig {
+
+    public static final String ORDER_CREATED_QUEUE = "orderms-order-created";
+}
