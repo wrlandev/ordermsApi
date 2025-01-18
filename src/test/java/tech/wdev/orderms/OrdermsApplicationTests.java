0593215cea218018e02309dce1d085d2fa0809fb
Wdev <wrlandev0@gmail.com>
2025-01-17 23:28:09 -0300

    first commit

---
+++ src/test/java/tech/wdev/orderms/OrdermsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.wdev.orderms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
