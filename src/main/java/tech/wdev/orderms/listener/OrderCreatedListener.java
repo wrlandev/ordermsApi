12ee09fa1bc0cb50868cdd0cdfa6ba445d284abe
Wdev <wrlandev0@gmail.com>
2025-02-04 17:51:52 -0300

    feat:create rabbitMQ queue listener in springBoot

---
+++ src/main/java/tech/wdev/orderms/listener/OrderCreatedListener.java
@@ -0,0 +1,22 @@
+package tech.wdev.orderms.listener;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+import tech.wdev.orderms.listener.dto.OrderCreatedEvent;
+
+import static tech.wdev.orderms.config.RabiitMqConfig.ORDER_CREATED_QUEUE;
+
+@Component
+public class OrderCreatedListener {
+
+    private final Logger logger = LoggerFactory.getLogger(OrderCreatedListener.class);
+
+    @RabbitListener(queues = ORDER_CREATED_QUEUE)
+    public void listen(Message<OrderCreatedEvent> message){
+    logger.info("Message consumed: {}", message);
+    }
+}
---
+++ src/main/java/tech/wdev/orderms/listener/dto/OrderCreatedEvent.java
@@ -0,0 +1,8 @@
+package tech.wdev.orderms.listener.dto;
+
+import java.util.List;
+
+public record OrderCreatedEvent(Long codigoPedido,
+                                Long codigoCliente,
+                                List<OrderItemEvent> itens) {
+}
